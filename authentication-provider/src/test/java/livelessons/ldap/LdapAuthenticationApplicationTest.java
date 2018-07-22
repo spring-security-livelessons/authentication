62972cc8356a7b4b069e94e18281df7ded9208e6
joshlong <josh@joshlong.com>
2018-07-22 15:34:19 -0700

    ap

--- authentication-provider/src/test/java/livelessons/ldap/LdapAuthenticationApplicationTest.java
+++ authentication-provider/src/test/java/livelessons/ldap/LdapAuthenticationApplicationTest.java
@@ -1,12 +1,9 @@
 package livelessons.ldap;
 
-import livelessons.custom.AuthenticationProviderApplication;
-import livelessons.custom.AuthenticationProviderApplicationTest;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.ldap.AutoConfigureDataLdap;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
@@ -19,8 +16,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
-
-@SpringBootTest(classes = {LdapAuthenticationApplication.class })
+@SpringBootTest(classes = {LdapAuthenticationApplication.class})
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("ldap")
